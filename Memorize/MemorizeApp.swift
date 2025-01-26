83ca9a7b6f29da339c221ebd56dcc2e4a383572d
NashidC <nash.chowdhury@gmail.com>
2025-01-26 03:49:53 -0500

    first commit on memorize game project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nashid  on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
