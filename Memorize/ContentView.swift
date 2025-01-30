78655198bbf30d58b480361917598e7b429b6554
NashidC <nash.chowdhury@gmail.com>
2025-01-30 01:02:04 -0500

    zstack updated, onTap gesture added

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -28,20 +28,20 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+   @State var isFaceUp: Bool = false
     var body: some View {
-        ZStack(content: {
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
             Text("🥰").font(.largeTitle)
                 
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
-                
-        })
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
